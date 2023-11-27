6ff1ce0d95428850c0fd8e288970c47469d87667
Ben Temessek iheb <iheb.bentemessek@esprit.tn>
2023-11-27 15:57:35 +0100

    ActivitySectorServiceAddTest

--- src/test/java/tn/esprit/devops_project/services/ActivitySectorImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/ActivitySectorImplTest.java
@@ -1,14 +1,42 @@
 package tn.esprit.devops_project.services;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.devops_project.entities.ActivitySector;
+import tn.esprit.devops_project.repositories.ActivitySectorRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ActivitySectorImplTest {
+    @InjectMocks
+    private ActivitySectorImpl activitySectorService;
+    @Mock
+    private ActivitySectorRepository activitySectorRepository;
+
     @Test
     void addActivitySector() {
+        // Create a sample ActivitySector object
+        ActivitySector sampleActivitySector = new ActivitySector();
+        sampleActivitySector.setIdSecteurActivite(1L);
+        sampleActivitySector.setCodeSecteurActivite("ABC");
+        sampleActivitySector.setLibelleSecteurActivite("Sample Sector");
+
+        // Mock the behavior of the repository save method
+        Mockito.when(activitySectorRepository.save(Mockito.any(ActivitySector.class))).thenReturn(sampleActivitySector);
+
+        // Call the service method to add the activity sector
+        ActivitySector addedActivitySector = activitySectorService.addActivitySector(sampleActivitySector);
+
+        // Assertions
+        assertNotNull(addedActivitySector);
+        assertEquals(1L, addedActivitySector.getIdSecteurActivite());
+        assertEquals("ABC", addedActivitySector.getCodeSecteurActivite());
+        assertEquals("Sample Sector", addedActivitySector.getLibelleSecteurActivite());
+
     }
 
     @Test
