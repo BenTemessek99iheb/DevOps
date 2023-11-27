c3e4298b4a67eddb690c2c7c3964b724828f4ccf
Ben Temessek iheb <iheb.bentemessek@esprit.tn>
2023-11-27 14:21:27 +0100

    Initial commit

---
+++ src/test/java/tn/esprit/devops_project/services/ActivitySectorImplTest.java
@@ -0,0 +1,29 @@
+package tn.esprit.devops_project.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ActivitySectorImplTest {
+    @Test
+    void addActivitySector() {
+    }
+
+    @Test
+    void retrieveAllActivitySectors() {
+    }
+
+    @Test
+    void deleteActivitySector() {
+    }
+
+    @Test
+    void updateActivitySector() {
+    }
+
+    @Test
+    void retrieveActivitySector() {
+    }
+}
