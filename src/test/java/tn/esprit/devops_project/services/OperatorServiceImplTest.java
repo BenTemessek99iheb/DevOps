7039a5ea081e119114b8b027b01ea15b7cb67acb
Ben Temessek iheb <iheb.bentemessek@esprit.tn>
2023-12-02 01:21:47 +0100

    Update OperatorServiceImplTest.java

--- src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
@@ -1,8 +1,6 @@
 package tn.esprit.devops_project.services;
 
 import org.junit.Before;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,11 +14,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class OperatorServiceImplTest {
