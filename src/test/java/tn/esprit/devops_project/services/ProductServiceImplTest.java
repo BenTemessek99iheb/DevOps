fc023ee518ab5f04d3cf0db12bcfd0e1e80c368b
Ben Temessek iheb <iheb.bentemessek@esprit.tn>
2023-12-02 01:21:24 +0100

    Update ProductServiceImplTest.java

--- src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
@@ -1,11 +1,56 @@
 package tn.esprit.devops_project.services;
+import org.junit.Before;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.devops_project.entities.Product;
+import tn.esprit.devops_project.entities.Stock;
+import tn.esprit.devops_project.repositories.ProductRepository;
+import tn.esprit.devops_project.repositories.StockRepository;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
 
 class ProductServiceImplTest {
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private StockRepository stockRepository;
 
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
     @Test
     void addProduct() {
+        Product product = new Product();
+        Stock stock = new Stock();
+        Long idStock = 1L;
+        when(stockRepository.findById(idStock)).thenReturn(Optional.of(stock));
+        when(productRepository.save(any(Product.class))).thenReturn(product);
+
+        // Act
+        Product savedProduct = productService.addProduct(product, idStock);
+
+        // Assert
+        assertNotNull(savedProduct);
+        assertEquals(stock, savedProduct.getStock());
+        verify(stockRepository).findById(idStock);
+        verify(productRepository).save(product);
+
     }
 
     @Test
